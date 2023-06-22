6b18b57f5bfbc5072b0ed6b8e015601640e7721e
Daniel Schwalbe Koda <schwalbekoda1@llnl.gov>
2023-06-22 11:07:40 -0700

    add pymatgen in file importer

--- mkite_db/dbimport/__init__.py
+++ mkite_db/dbimport/__init__.py
@@ -2,5 +2,6 @@ from .base import DbImporter
 from .mp import MPImporter
 from .molfile import MolFileImporter
 from .asefile import AseFileImporter
+from .pmgfile import PymatgenFileImporter
 
-__all__ = [MPImporter, MolFileImporter, AseFileImporter]
+__all__ = [MPImporter, MolFileImporter, AseFileImporter, PymatgenFileImporter]
