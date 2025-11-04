0a1c22c4f451cb906d5cc2d4edbf4d64fedd994b
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-11-03 19:33:58 -0800

    delete calcs app

--- mkite_db/orm/calcs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalcsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mkite_db.orm.calcs"
--- mkite_db/orm/calcs/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.postgres.fields import ArrayField
-
-from mkite_db.orm.repr import _named_repr
-from mkite_db.orm.base.models import CalcNode, DbEntry
